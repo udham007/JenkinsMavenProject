76c03bd535f9582f9f718dc0d2829d928dd0da74
udham007 <66781898+udham007@users.noreply.github.com>
2021-05-30 09:26:17 +0530

    Update SampleSeleniumTest.java

--- src/test/java/com/renju/training/jenkins/SampleSeleniumTest.java
+++ src/test/java/com/renju/training/jenkins/SampleSeleniumTest.java
@@ -47,44 +47,77 @@ public class SampleSeleniumTest {
 package com.renju.training.jenkins;
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class SampleSeleniumTest {
+public class NewTest {
+
+     boolean flag = true;
     public WebDriver driver;
     public String baseurl = "http://google.com";
 
-    @BeforeMethod
-    public void launchbrowser()
+
+
+    @BeforeTest
+     public void launchbrowser()
 
     {
         System.out.println("LaunchBrowser called...");
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get(baseurl);
+    }
+
+
+    @BeforeMethod
+    public void Beforemethod() {
+        System.out.println("Hello Before Method");
     }
 
     @Test(priority = 0)
     public void GmailTap() {
         System.out.println("Test priority 0 called...");
-        driver.findElement(By.linkText("Gmail")).click();
-
+        driver.findElement(By.xpath("//*[@id=\"gbw\"]/div/div/div[1]/div[1]/a")).click();
         System.out.println("hello");
         driver.findElement(By.id("Email")).sendKeys("virendra.cbo@gmail.com");
-        driver.findElement(By.id("next")).click();
-
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.HOURS);
+        driver.findElement(By.name("signIn")).click();
         boolean flag = true;
-
         while (flag) {
+            try {
+                driver.findElement(By.name("Passwd")).sendKeys("Qwerty@123");
+                System.out.println("Executed");
+                flag = false;
+            } catch (Exception e) {
+            }
+        }
 
+        driver.findElement(By.id("signIn")).click();
+        driver.findElement(By.xpath("//*[@id=\"gb\"]/div[1]/div[1]/div[2]/div[4]/div[1]/a/span")).click();
+        driver.findElement(By.xpath("//*[@id=\"gb_71\"]")).click();
+
+    }
+
+
+
+
+
+    @Test(priority = 1)
+    public void GmailTap2() {
+        System.out.println("Test priority 1 called...");
+        System.out.println("hello");
+        driver.findElement(By.id("Email")).sendKeys("virendra.cbo@gmail.com");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.HOURS);
+        driver.findElement(By.name("signIn")).click();
+        boolean flag = true;
+        while (flag) {
             try {
                 driver.findElement(By.name("Passwd")).sendKeys("Qwerty@123");
                 System.out.println("Executed");
@@ -100,47 +133,22 @@ public class SampleSeleniumTest {
 
     }
 
-    @Test(priority = 1)
-    public void GmailTap2() {
-        System.out.println("Test priority 1 called...");
-        System.out.println("Test priority 0 called...");
-        driver.findElement(By.linkText("Gmail")).click();
-
-        System.out.println("hello");
-        driver.findElement(By.id("Email")).sendKeys("virendra.cbo@gmail.com");
-        driver.findElement(By.id("next")).click();
-
-        boolean flag = true;
-        while (flag) {
-            try {
-                driver.findElement(By.name("Passwd")).sendKeys("Qwerty@123");
-                System.out.println("Executed");
-                flag = false;
-            } catch (Exception e) {
-            }
-        }
-
-        driver.findElement(By.id("signIn")).click();
-
-        WebElement myDynamicElement = (new WebDriverWait(driver, 10)).until(ExpectedConditions
-                .presenceOfElementLocated(By.xpath("//*[@id=\"gb\"]/div[1]/div[1]/div[2]/div[4]/div[1]/a/span")));
-        myDynamicElement.click(); // driver.findElement(By.xpath("//*[@id=\"gb\"]/div[1]/div[1]/div[2]/div[4]/div[1]/a/span")).click();
-                                    // driver.findElement(By.xpath("//*[@id=\"gb_71\"]")).click();
-
-    }
-
-    // ____________
 
     @AfterMethod
-    public void aftersignout() {
-        System.out.println("aftersignOut called..."); // New Accnt
+    public void aftersignout1() {
 
-        WebElement myDynamicElement = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"account-chooser-link\"]")));
-        myDynamicElement.click();
-
+        if (flag)
+         {
+            driver.findElement(By.xpath("//*[@id=\"account-chooser-link\"]")).click();
+         flag = false;
+         }
         driver.findElement(By.xpath("//*[@id=\"account-chooser-add-account\"]")).click();
-        driver.quit();
-    } // *[@id="account-chooser-add-account"]
-
-}
+    }
+    //*[@id="account-chooser-add-account"]
+    @AfterTest
+    public void aftersignout() {
+        System.out.println("aftersignOut called...");
+        // New Accnt`enter code here`
+        driver.close();
+    }
+    }
